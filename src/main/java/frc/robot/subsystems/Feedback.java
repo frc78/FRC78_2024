31a0154303ec746cdd1d34526ae646fd79641ff7
siebs7317 <35415103+siebs7317@users.noreply.github.com>
2024-02-06 12:39:15 +0000

    spotless

--- src/main/java/frc/robot/subsystems/Feedback.java
+++ src/main/java/frc/robot/subsystems/Feedback.java
@@ -5,28 +5,28 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.led.CANdle;
-
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Feedback extends SubsystemBase {
   /** Creates a new Feedback. */
-  private CANdle bracelet; 
+  private CANdle bracelet;
+
   public Feedback(int candleID) {
     bracelet = new CANdle(candleID);
   }
 
   public void red() {
-    bracelet.setLEDs(255, 0, 0); 
+    bracelet.setLEDs(255, 0, 0);
   }
 
   public void multi(Color color) {
-    bracelet.setLEDs(((int)(color.red*255)), ((int)(color.green*255)), ((int)(color.blue*255))); 
-
+    bracelet.setLEDs(
+        ((int) (color.red * 255)), ((int) (color.green * 255)), ((int) (color.blue * 255)));
   }
 
   public void off() {
-    bracelet.setLEDs(0, 0, 0); 
+    bracelet.setLEDs(0, 0, 0);
   }
 
   @Override
@@ -37,8 +37,8 @@ public class Feedback extends SubsystemBase {
 
 /*   Can use this to add feedback light to buttons
  *   new Trigger(m_driveController::getStartButton).onTrue(new InstantCommand(() -> m_chassis.resetPose(new Pose2d())));
- *   new Trigger(m_driveController::getBButton).whileTrue(Commands.startEnd(m_feedback::red, m_feedback::off, m_feedback)); 
- *   new Trigger(m_driveController::getXButton).whileTrue(Commands.startEnd(()->m_feedback.multi(new Color(186,7,162)), m_feedback::off, m_feedback)); 
- *   new Trigger(m_driveController::getYButton).whileTrue(Commands.startEnd(()->m_feedback.multi(Color.fromHSV(316,47,10)), m_feedback::off, m_feedback)); 
+ *   new Trigger(m_driveController::getBButton).whileTrue(Commands.startEnd(m_feedback::red, m_feedback::off, m_feedback));
+ *   new Trigger(m_driveController::getXButton).whileTrue(Commands.startEnd(()->m_feedback.multi(new Color(186,7,162)), m_feedback::off, m_feedback));
+ *   new Trigger(m_driveController::getYButton).whileTrue(Commands.startEnd(()->m_feedback.multi(Color.fromHSV(316,47,10)), m_feedback::off, m_feedback));
  *   new Trigger(m_driveController::getAButton).whileTrue(Commands.startEnd(()->m_feedback.multi(new Color(0,255,0)), m_feedback::off, m_
  */
