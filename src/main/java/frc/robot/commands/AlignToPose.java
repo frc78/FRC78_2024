32b78fb72dec1c4b681e133051d21b9dea241dbf
Nehuel Armenanzas <74217788+uncookednehuel@users.noreply.github.com>
2024-02-10 18:00:31 -0500

    Started creating aligning pose estimator

---
+++ src/main/java/frc/robot/commands/AlignToPose.java
@@ -0,0 +1,11 @@
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.chassis.Chassis;
+import frc.robot.subsystems.chassis.PoseEstimator;
+
+public class AlignToPose extends Command {
+  public AlignToPose(Chassis chassis, Pose2d pose, PoseEstimator poseEstimator) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    // Configure the command by calling addRequirements(chassis)
+  }
+}
