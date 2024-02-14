2c076a68a7672709e69f52db327cc45a26850d10
morgan <151097610+morgan818@users.noreply.github.com>
2024-02-14 18:33:50 -0500

    Rumble (#93)
    Co-authored-by: lizrobots <estevens3564@gmail.com>

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -58,6 +58,10 @@ public class Shooter extends SubsystemBase {
     shooterBottomVV = new VelocityVoltage(0).withSlot(0);
   }
 
+  public boolean isAtSpeed(double threshold) {
+    return (shooterTOP.getVelocity().getValueAsDouble() >= shooterTopVV.Velocity * threshold);
+  }
+
   public void setPIDReferenceTOP(double setPoint) {
     shooterTOP.setControl(
         shooterTopVV.withVelocity(setPoint / 60).withFeedForward(config.flywheelTopFF.kFF));
