369be4edcbb756c9c698cdb41e20d4e84d66175e
Tim Winters <twinters007@gmail.com>
2024-09-03 19:28:30 -0400

    Update wrist gear ratio and add sysid

--- src/main/java/frc/robot/competition/Robot.java
+++ src/main/java/frc/robot/competition/Robot.java
@@ -8,7 +8,6 @@ import com.ctre.phoenix6.SignalLogger;
 import com.reduxrobotics.canand.CanandEventLoop;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.net.PortForwarder;
-import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.PowerDistribution;
@@ -16,14 +15,12 @@ import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import java.util.HashMap;
 import org.littletonrobotics.junction.LogFileUtil;
 import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.networktables.NT4Publisher;
 import org.littletonrobotics.junction.wpilog.WPILOGReader;
 import org.littletonrobotics.junction.wpilog.WPILOGWriter;
-import org.littletonrobotics.urcl.URCL;
 
 public class Robot extends LoggedRobot {
   private Command m_autonomousCommand;
@@ -53,7 +50,6 @@ public class Robot extends LoggedRobot {
     PortForwarder.add(5800, "limelight.local", 5800);
     PortForwarder.add(1181, "photonvision.local", 1181);
     if (isReal()) {
-      DataLogManager.start();
       Logger.addDataReceiver(new WPILOGWriter()); // Log to a USB stick ("/U/logs")
       Logger.addDataReceiver(new NT4Publisher()); // Publish data to NetworkTables
       new PowerDistribution(1, ModuleType.kRev); // Enables power distribution logging
@@ -69,27 +65,8 @@ public class Robot extends LoggedRobot {
     }
 
     Logger.start();
-    URCL.start(
-        new HashMap<>() {
-          {
-            put(1, "Front Left Drive");
-            put(2, "Front Left Steer");
-            put(3, "Front Right Drive");
-            put(4, "Front Right Steer");
-            put(5, "Back Left Drive");
-            put(6, "Back Left Steer");
-            put(7, "Back Right Drive");
-            put(8, "Back Right Steer");
-            put(9, "Intake Bottom");
-            put(10, "Intake Top");
-            put(11, "Elevator 11");
-            put(12, "Elevator 12");
-            put(13, "Wrist");
-          }
-        });
     // CTRE logger
-    SignalLogger.setPath("/U/ctre-logs/");
-    SignalLogger.start();
+    SignalLogger.setPath("/u/ctre-logs/");
     m_robotContainer = new CompetitionRobotContainer();
 
     Notifier poseNotifier = new Notifier(m_robotContainer.m_poseEstimator::update);
