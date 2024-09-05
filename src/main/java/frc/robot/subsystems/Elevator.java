82725555aee3d6e84068854dd09e28fcff9a1d64
Landon Montecalvo <115672301+ljmontecalvo@users.noreply.github.com>
2024-09-04 20:33:56 -0400

    Elevator Works
    Finished the elevator code, it was tested and is functional.

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -4,33 +4,27 @@
 
 package frc.robot.subsystems;
 
-import static edu.wpi.first.units.Units.InchesPerSecond;
-import static edu.wpi.first.units.Units.MetersPerSecond;
-import static edu.wpi.first.units.Units.Second;
-
 import com.ctre.phoenix.motorcontrol.*;
+import com.ctre.phoenix6.SignalLogger;
 import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.ctre.phoenix6.signals.ReverseLimitValue;
 
-import edu.wpi.first.math.controller.ElevatorFeedforward;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.units.Distance;
-import edu.wpi.first.units.Measure;
-import edu.wpi.first.units.Velocity;
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+
 import org.littletonrobotics.junction.Logger;
 
 public class Elevator extends SubsystemBase {
@@ -46,34 +40,25 @@ public class Elevator extends SubsystemBase {
     return !zeroed;
   }
 
-  private static final double kS = 0.070936;
-  private static final double kV = 0.79005;
-  private static final double kA = 0.086892;
-  private static final double kG = 0.088056;
-  // Command loop runs at 50Hz, 20ms period
-  private static final double kDt = 0.02;
-  private double appliedOutput = 0;
-
   public Elevator() {
     leader = new TalonFX(11, "*");
     follower = new TalonFX(12, "*");
 
     TalonFXConfiguration configs = new TalonFXConfiguration();
-    configs.Feedback.SensorToMechanismRatio = (1.29 * Math.PI) / 25;
+    configs.Feedback.SensorToMechanismRatio = 25 / (1.29 * Math.PI);
     configs.SoftwareLimitSwitch.ForwardSoftLimitEnable = false;
     configs.SoftwareLimitSwitch.ReverseSoftLimitEnable = false;
     configs.SoftwareLimitSwitch.ForwardSoftLimitThreshold = 0;
-    configs.Slot0.kP = 1;
+    configs.Slot0.kP = 66.84;
     configs.Slot0.kI = 0;
-    configs.Slot0.kD = 10;
-    configs.Slot0.kV = 2;
-    configs.Slot0.kS = 0.070936;
-    configs.Slot0.kV = 0.79005;
-    configs.Slot0.kA = 0.086892;
-    configs.Slot0.kG = 0.088056;
+    configs.Slot0.kD = 1.7421;
+    configs.Slot0.kS = 0.22964;
+    configs.Slot0.kV = 0.70964;
+    configs.Slot0.kA = 0.018805;
+    configs.Slot0.kG = 0.12011;
     configs.MotionMagic.MotionMagicAcceleration = 80f;
     configs.MotionMagic.MotionMagicCruiseVelocity = 15f;
-    configs.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+    configs.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
 
     leader.getConfigurator().apply(configs);
     follower.setControl(new Follower(11, true));
@@ -85,16 +70,10 @@ public class Elevator extends SubsystemBase {
 
     // leader.setInverted(false);
     // follower.Follow(11, false);
-
-    follower.setControl(new Follower(11, false));
-
     // Util.setRevStatusRates(leader, 5, 20, 20, 32767, 32767, 32767, 32767, 32767);
     // Util.setRevStatusRates(follower, 500, 32767, 32767, 32767, 32767, 32767, 32767, 32767);
 
     leader.getPosition().setUpdateFrequency(50);
-    leader.optimizeBusUtilization();
-
-    follower.optimizeBusUtilization();
 
     this.setDefaultCommand(setToTarget(0));
     SmartDashboard.putData(enableCoastMode());
@@ -109,18 +88,24 @@ public class Elevator extends SubsystemBase {
   public boolean elevIsAtPos() {
     var actualPos = leader.getPosition().getValue();
     var targetPos = control.Position;
-    
+
     return Math.abs(actualPos - targetPos) <= 0.1f;
   }
 
   private Command lowerElevatorUntilLimitReached() {
-    return run(() -> leader.set(-.1)).until(() -> leader.getReverseLimit().getValue() == ReverseLimitValue.ClosedToGround);
+    return run(() -> leader.set(-.1))
+        .until(() -> leader.getReverseLimit().getValue() == ReverseLimitValue.ClosedToGround);
   }
 
   private Command configureMotorsAfterZeroing() {
     return runOnce(
             () -> {
-              var limitConfigs = new SoftwareLimitSwitchConfigs().withForwardSoftLimitEnable(true).withForwardSoftLimitThreshold(17f).withReverseSoftLimitEnable(true).withReverseSoftLimitThreshold(0);
+              var limitConfigs =
+                  new SoftwareLimitSwitchConfigs()
+                      .withForwardSoftLimitEnable(true)
+                      .withForwardSoftLimitThreshold(16f)
+                      .withReverseSoftLimitEnable(true)
+                      .withReverseSoftLimitThreshold(0);
 
               leader.getConfigurator().apply(limitConfigs);
 
@@ -161,7 +146,9 @@ public class Elevator extends SubsystemBase {
   }
 
   public void periodic() {
-    Logger.recordOutput("Elevator/limit pressed", leader.getReverseLimit().getValue() == ReverseLimitValue.ClosedToGround);
+    Logger.recordOutput(
+        "Elevator/limit pressed",
+        leader.getReverseLimit().getValue() == ReverseLimitValue.ClosedToGround);
     Logger.recordOutput("Elevator/zeroed", zeroed);
     Logger.recordOutput("Elevator/position", leader.getPosition().getValue());
   }
@@ -172,11 +159,41 @@ public class Elevator extends SubsystemBase {
 
   /** Moves elevator to target as long as elevator is zeroed */
   public Command setToTarget(double target) {
-    return runOnce(() -> {
-      if (zeroed) {
-        control.Position = target;
-        leader.setControl(control);
-      }
-    }).withName("setTo[" + target + "]");
+    return runOnce(
+            () -> {
+              if (zeroed) {
+                control.Position = target;
+                leader.setControl(control);
+              }
+            }).andThen(Commands.idle())
+        .withName("setTo[" + target + "]");
+  }
+
+  private final TalonFX m_motor = new TalonFX(0);
+  private final VoltageOut m_voltReq = new VoltageOut(0.0);
+
+  private final SysIdRoutine m_sysIdRoutine =
+    new SysIdRoutine(
+        new SysIdRoutine.Config(
+          null,        // Use default ramp rate (1 V/s)
+          Units.Volts.of(4), // Reduce dynamic step voltage to 4 to prevent brownout
+          null,        // Use default timeout (10 s)
+                        // Log state with Phoenix SignalLogger class
+          (state) -> SignalLogger.writeString("state", state.toString())
+        ),
+        new SysIdRoutine.Mechanism(
+          (volts) -> leader.setControl(m_voltReq.withOutput(volts.in(Units.Volts))),
+          null,
+          this
+        )
+    );
+
+  public Command runSysId() {
+    return Commands.sequence(runOnce(SignalLogger::start),
+    m_sysIdRoutine.quasistatic(Direction.kForward).until(() -> leader.getFault_ForwardSoftLimit().getValue()), 
+    m_sysIdRoutine.quasistatic(Direction.kReverse).until(() -> leader.getReverseLimit().getValue() == ReverseLimitValue.ClosedToGround), 
+    m_sysIdRoutine.dynamic(Direction.kForward).until(() -> leader.getFault_ForwardSoftLimit().getValue()), 
+    m_sysIdRoutine.dynamic(Direction.kReverse).until(() -> leader.getReverseLimit().getValue() == ReverseLimitValue.ClosedToGround),
+    runOnce(SignalLogger::stop));
   }
 }
